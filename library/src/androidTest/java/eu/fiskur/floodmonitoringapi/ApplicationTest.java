6fe4bf15bcaf2a75d9b423b7a5e562e15cdd767d
Jonathan Fisher <jonathan@fiskur.eu>
2016-02-16 13:47:14 +0000

    adding jacoco

--- library/src/androidTest/java/eu/fiskur/floodmonitoringapi/ApplicationTest.java
+++ library/src/androidTest/java/eu/fiskur/floodmonitoringapi/ApplicationTest.java
@@ -3,11 +3,17 @@ package eu.fiskur.floodmonitoringapi;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import org.junit.Assert;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testTest() {
+        Assert.assertEquals("Hello", "Hello");
     }
 }
