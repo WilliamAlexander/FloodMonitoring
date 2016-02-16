4c991812e598a5974541bf16d07765e952e9fa21
Jonathan Fisher <jonathan@fiskur.eu>
2016-02-16 16:02:38 +0000

    forecast risk full coverage

--- library/src/test/java/eu/fiskur/floodmonitoringapi/ThreeDayForecastTests.java
+++ library/src/test/java/eu/fiskur/floodmonitoringapi/ThreeDayForecastTests.java
@@ -4,6 +4,10 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.fiskur.floodmonitoringapi.alerts.ForecastRisk;
 import eu.fiskur.floodmonitoringapi.alerts.ThreeDayForecast;
 
 public class ThreeDayForecastTests {
@@ -30,5 +34,32 @@ public class ThreeDayForecastTests {
         Assert.assertEquals("summary_welsh", forecast.getForecastSummaryWelsh());
         Assert.assertEquals("issue_date_time", forecast.getIssueDatetime());
         Assert.assertEquals("label", forecast.getLabel());
+
+        ForecastRisk risk = new ForecastRisk();
+        risk.setId("id1");
+        List<ForecastRisk> risks = new ArrayList<>();
+        risks.add(risk);
+        forecast.setForecastRisk(risks);
+
+        Assert.assertEquals("id1", forecast.getForecastRisk().get(0).getId());
+    }
+
+    @Test
+    public void testToString(){
+        ThreeDayForecast forecast = new ThreeDayForecast();
+        forecast.setType("type");
+        forecast.setId("id");
+        forecast.setDay1image("image_1");
+        forecast.setDay2image("image_2");
+        forecast.setDay3image("image_3");
+        forecast.setForecastSummary("summary");
+        forecast.setForecastSummaryWelsh("summary_welsh");
+        forecast.setIssueDatetime("issue_date_time");
+        forecast.setLabel("label");
+
+        Assert.assertEquals("@id: id\n" +
+                "forecastSummary: summary\n" +
+                "issueDatetime: issue_date_time\n" +
+                "type: type\n", forecast.toString());
     }
 }
